1b703aea15fdd81a152888c54ebd9823b513f882
Andreas Hechenberger <world@greenstation.de>
2018-07-26 19:20:54 +0200

    add basic http auth for dummy users and restrict access to CREATE,UPDATE,DELETE calls

---
+++ src/main/java/me/hechenberger/employee/configuration/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package me.hechenberger.employee.configuration;
+
+import me.hechenberger.employee.security.AuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private AuthenticationEntryPoint entryPoint;
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    PasswordEncoder encoder = new BCryptPasswordEncoder();
+    //add dummy entries
+    auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("admin").password(encoder.encode("1234")).roles("ADMIN");
+    auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("user").password(encoder.encode("1234")).roles("USER");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // allow all request except those how change an employee
+    http
+            .csrf().disable()
+            .formLogin().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .httpBasic()
+            .authenticationEntryPoint(entryPoint)
+            .and()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/api/v1/employee/**").permitAll()
+            .antMatchers(HttpMethod.POST, "/api/v1/employee/**").authenticated()
+            .antMatchers(HttpMethod.PUT, "/api/v1/employee/**").authenticated()
+            .antMatchers(HttpMethod.DELETE, "/api/v1/employee/**").authenticated()
+            .anyRequest().permitAll(); // ex. allow to call swagger-ui
+  }
+}
--- src/test/java/security/AuthenticationTest.java
+++ src/test/java/security/AuthenticationTest.java
@@ -1,8 +1,10 @@
-package controller;
+package security;
 
 // for simple testing
 
 import static org.assertj.core.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import me.hechenberger.employee.ApplicationStart;
 import me.hechenberger.employee.model.Employee;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,10 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -31,18 +34,19 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.LinkedList;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = ApplicationStart.class)
+@WebAppConfiguration
 @AutoConfigureMockMvc
-public class EmployeeControllerTest {
+public class AuthenticationTest {
 
   private static MediaType CONTENTTYPE = new MediaType(MediaType.APPLICATION_JSON.getType(),
           MediaType.APPLICATION_JSON.getSubtype(),
           Charset.forName("utf8"));
+
 
   @Autowired
   private MockMvc mockMvc;
@@ -54,9 +58,9 @@ public class EmployeeControllerTest {
   // api endpoint
   private static String apiUrl = "/api/v1/employee/";
 
-
-  // list to keep track of saved and deleted entries
-  private static LinkedList<Employee> EMPLOYEELIST;
+  public static RequestPostProcessor allowedUser() {
+    return user("admin").password("1234");
+  }
 
   @Autowired
   void setConverters(HttpMessageConverter<?>[] converters) {
@@ -72,70 +76,49 @@ public class EmployeeControllerTest {
   // setup before each test
   @Before
   public void setUp() throws Exception {
-    this.mockMvc = MockMvcBuilders
-            .webAppContextSetup(webApplicationContext)
+    this.mockMvc = MockMvcBuilders.
+            webAppContextSetup(webApplicationContext)
+            .apply(springSecurity())
             .build();
   }
 
-  // setup before suite (class) starts
-  @BeforeClass
-  public static void setupSuite() throws Exception {
-    EMPLOYEELIST = new LinkedList<>();
+  @Test
+  public void testPermissions() throws Exception {
+    // post
+    Employee employee = new Employee("jane.doe" + Math.random() + "@example.com", "Jane" + Math.random(), "Doe", new Date(), new String[]{"soccer", "music", "dance"});
+    mockMvc.perform(post(apiUrl).content(this.json(employee)).contentType(CONTENTTYPE)).andExpect(status().isUnauthorized());
+    ResultActions res = mockMvc.perform(post(apiUrl).content(this.json(employee)).contentType(CONTENTTYPE).with(allowedUser())).andExpect(status().isCreated());
+    Employee stored = readObj(Employee.class, res);
+
+    // get
+    mockMvc.perform(get(apiUrl).contentType(CONTENTTYPE)).andExpect(status().isOk());
+    mockMvc.perform(get(apiUrl + stored.getId()).contentType(CONTENTTYPE)).andExpect(status().isOk());
+
+    mockMvc.perform(get(apiUrl).contentType(CONTENTTYPE).with(allowedUser())).andExpect(status().isOk());
+    mockMvc.perform(get(apiUrl + stored.getId()).contentType(CONTENTTYPE).with(allowedUser())).andExpect(status().isOk());
+
+    // update
+    stored.setFirstName(stored.getFirstName() + Math.random());
+    mockMvc.perform(put(apiUrl).content(this.json(stored)).contentType(CONTENTTYPE)).andExpect(status().isUnauthorized());
+    mockMvc.perform(put(apiUrl).content(this.json(stored)).contentType(CONTENTTYPE).with(allowedUser())).andExpect(status().isOk());
+
+    // delete
+    mockMvc.perform(delete(apiUrl).content(this.json(stored)).contentType(CONTENTTYPE)).andExpect(status().isUnauthorized());
+    mockMvc.perform(delete(apiUrl).content(this.json(stored)).contentType(CONTENTTYPE).with(allowedUser())).andExpect(status().isOk());
   }
 
-  @Test
-  public void createEmployee() throws Exception {
-    Employee dummy = getRandomDummy();
-    Employee stored = createStoreEmployee(dummy);
-    assertThat(stored).isNotNull();
-    assertThat(stored).isNotEqualTo(dummy);
-    assertThat(stored.getId()).isGreaterThan(0);
-    // check for second try
-    mockMvc.perform(post(apiUrl).content(this.json(dummy)).contentType(CONTENTTYPE)).andExpect(status().isNotAcceptable());
-  }
-
-  @Test
-  public void updateEmployee() throws Exception {
-    Employee stored = createStoreEmployee(getRandomDummy());
-    stored.setFirstName(stored.getFirstName() + " " + Math.random());
-    mockMvc.perform(put(apiUrl).content(this.json(stored)).contentType(CONTENTTYPE)).andExpect(status().isOk());
-    stored = (Employee) stored.clone(); // id is set to 0 by cloning it
-    mockMvc.perform(put(apiUrl).content(this.json(stored)).contentType(CONTENTTYPE)).andExpect(status().isNotAcceptable());
-  }
-
-  @Test
-  public void deleteEmployee() throws Exception {
-    Employee stored = createStoreEmployee(getRandomDummy());
-    mockMvc.perform(delete(apiUrl).content(this.json(stored)).contentType(CONTENTTYPE)).andExpect(status().isOk());
-    EMPLOYEELIST.remove(stored);
-  }
-
-  @Test
-  public void getAllEmployee() throws Exception {
-    createStoreEmployee(getRandomDummy());
-    ResultActions res = mockMvc.perform(get(apiUrl).contentType(CONTENTTYPE)).andExpect(status().isOk());
-    Employee[] list = readObj(Employee[].class,res);
-    assertThat(list.length).isEqualTo(EMPLOYEELIST.size());
-  }
-
-  @Test
-  public void getEmployee() throws Exception {
-    Employee stored = createStoreEmployee(getRandomDummy());
-    ResultActions res = mockMvc.perform(get(apiUrl + stored.getId()).contentType(CONTENTTYPE)).andExpect(status().isOk());
-    Employee returnedEmployee = readObj(Employee.class,res);
-    assertThat(stored).isEqualTo(returnedEmployee);
-  }
 
   // ##############################################################################################
   // ############# HELPER FUNCTIONS
   // ##############################################################################################
 
-  private <T> T readObj(Class clazz, ResultActions res) throws Exception{
+  private <T> T readObj(Class clazz, ResultActions res) throws Exception {
     return new ObjectMapper().readerFor(clazz).readValue(res.andReturn().getResponse().getContentAsString());
   }
 
   /**
    * converts an object to an json string
+   *
    * @param o
    * @return corresponding json string of object
    */
@@ -151,27 +134,4 @@ public class EmployeeControllerTest {
     return null;
   }
 
-  /**
-   * create a dummy with a random name and email
-   * @return employee object
-   */
-  private Employee getRandomDummy() {
-    return new Employee("jane.doe" + Math.random() + "@example.com", "Jane" + Math.random(), "Doe", new Date(), new String[]{"soccer", "music", "dance"});
-  }
-
-  /**
-   * store an employee via the api and save it into a list to check adding updating and remove
-   * @param employee
-   * @return corresponding employee, if the id is not null, the employee was already stored
-   * @throws Exception
-   */
-  private Employee createStoreEmployee(Employee employee) throws Exception {
-    if (employee.getId() == null) {
-      ResultActions res = mockMvc.perform(post(apiUrl).content(this.json(employee)).contentType(CONTENTTYPE)).andExpect(status().isCreated());
-      Employee stored = readObj(Employee.class,res);
-      EMPLOYEELIST.add(stored);
-      return stored;
-    }
-    return employee;
-  }
 }
